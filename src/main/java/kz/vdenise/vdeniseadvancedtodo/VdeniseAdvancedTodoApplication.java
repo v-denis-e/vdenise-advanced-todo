d70d76796b96ed117a903c927a39459fc06a799c
valyayev.denis <denis.valyaev@asia-soft.kz>
2021-01-07 08:04:21 +0600

    Проинициализирован Spring Boot проект. closes #1

---
+++ src/main/java/kz/vdenise/vdeniseadvancedtodo/VdeniseAdvancedTodoApplication.java
@@ -0,0 +1,13 @@
+package kz.vdenise.vdeniseadvancedtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdeniseAdvancedTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdeniseAdvancedTodoApplication.class, args);
+	}
+
+}
