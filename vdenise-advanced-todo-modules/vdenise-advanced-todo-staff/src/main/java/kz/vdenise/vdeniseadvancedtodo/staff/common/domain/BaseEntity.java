06bd01bfdaed5e32f3689c1af3ed373087e4d2a5
valyayev.denis <denis.valyaev@asia-soft.kz>
2021-01-07 09:18:47 +0600

    Добавлен суперкласс для всех сущностей. closes #8

---
+++ vdenise-advanced-todo-modules/vdenise-advanced-todo-staff/src/main/java/kz/vdenise/vdeniseadvancedtodo/staff/common/domain/BaseEntity.java
@@ -0,0 +1,12 @@
+package kz.vdenise.vdeniseadvancedtodo.staff.common.domain;
+
+import java.time.LocalDateTime;
+
+public class BaseEntity {
+
+    private Long id;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+
+}
